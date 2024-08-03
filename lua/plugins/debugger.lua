59f6c93512e8b41c14a4ca7f9a14f207e358517c
tryprncp <tryprncp@gmail.com>
2024-08-03 22:44:16 +0800

    Lazyloaded the nvim-dap plugin on keypress

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,6 +1,10 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  lazy = true,
+  keys = {
+    { '<F7>' },
+  },
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
