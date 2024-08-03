345dbe387ee029ff96f4c82eacf164caf7fce71c
tryprncp <tryprncp@gmail.com>
2024-08-03 22:35:12 +0800

    Added dashboard-nvim plugin

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -1,5 +1,57 @@
 return {
   {
+    'nvimdev/dashboard-nvim',
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    opts = function()
+      local logo = [[
+_____  ___    _______    ______  ___      ___  __     ___      ___
+(\"   \|"  \  /"     "|  /    " \|"  \    /"  ||" \   |"  \    /"  |
+|.\\   \    |(: ______) // ____  \\   \  //  / ||  |  |\   \  //   |
+|: \.   \\  | \/    |  /  /    ) :)\\  \/. ./  |:  |  | \\  \/.    |
+|.  \    \. | // ___)_(: (____/ //  \.    //   |.  |  |: \.        |
+|    \    \ |(:      "|\        /    \\   /    /\  |\ |.  \    /:  |
+ \___|\____\) \_______) \"_____/      \__/    (__\_|_)|___|\__/|___|
+
+https://github.com/tryprncp/neovim
+      ]]
+
+      logo = string.rep('\n', 15) .. logo .. '\n\n'
+
+      local opts = {
+        theme = 'doom',
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = true,
+        },
+        config = {
+          header = vim.split(logo, '\n'),
+          -- stylua: ignore
+          center = {
+            { action = 'Lexplore',          desc = " Open file explorer",   icon = " ", key = "\\" },
+            { action = 'e $MYVIMRC',        desc = " Edit neovim config",   icon = "󰈮 ", key = "e" },
+            { action = 'Mason',             desc = " Mason",                icon = "󱘊 ", key = "m" },
+            { action = "Lazy",              desc = " Lazy",                 icon = "󰒲 ", key = "l" },
+            { action = 'qa',                desc = " Quit",                 icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+        button.key_format = '  %s'
+      end
+
+      return opts
+    end,
+  },
+
+  {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     opts = {
