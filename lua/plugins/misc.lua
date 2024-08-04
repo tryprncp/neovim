df5a9f36c53478253cb54501b4ab5855ba40a376
tryprncp <tryprncp@gmail.com>
2024-08-04 11:21:09 +0800

    Enabled mini.hipatterns module

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -79,14 +79,14 @@ return {
     event = 'UIEnter',
     config = function()
       require('mini.misc').setup()
-      -- require('mini.hipatterns').setup {
-      --   highlighters = {
-      --     fixme = { pattern = 'FIXME', group = 'MiniHipatternsFixme' },
-      --     hack = { pattern = 'HACK', group = 'MiniHipatternsHack' },
-      --     todo = { pattern = 'TODO', group = 'MiniHipatternsTodo' },
-      --     note = { pattern = 'NOTE', group = 'MiniHipatternsNote' },
-      --   },
-      -- }
+      require('mini.hipatterns').setup {
+        highlighters = {
+          fixme = { pattern = 'FIXME', group = 'MiniHipatternsFixme' },
+          hack = { pattern = 'HACK', group = 'MiniHipatternsHack' },
+          todo = { pattern = 'TODO', group = 'MiniHipatternsTodo' },
+          note = { pattern = 'NOTE', group = 'MiniHipatternsNote' },
+        },
+      }
       -- Better Around/Inside textobjects
       --
       -- Examples:
