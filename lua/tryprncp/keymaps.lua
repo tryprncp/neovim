12f54ce5bbcbd89b9d8196872506407cbd03fd81
tryprncp <tryprncp@gmail.com>
2024-08-07 01:34:49 +0800

    Refactored the keymaps

--- lua/tryprncp/keymaps.lua
+++ lua/tryprncp/keymaps.lua
@@ -1,70 +1,85 @@
 --[[ KEYMAPS ]]
--- See :help vim.keymap
+local set = vim.keymap.set
+
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+set({ 'n', 'v' }, '<Space>', '<Nop>')
 
 -- Toggle relative line number
-vim.keymap.set('n', '<leader>ln', '<cmd>set invrelativenumber<cr>', { desc = 'Toggle relative line number' })
+set('n', '<leader>ln', '<cmd>set invrelativenumber<cr>', { desc = 'Toggle relative line number' })
 
 -- Toggle line wrap
-vim.keymap.set('n', '<leader>lw', '<cmd>set invwrap<cr>', { desc = 'Toggle line wrap' })
+set('n', '<leader>lw', '<cmd>set invwrap<cr>', { desc = 'Toggle line wrap' })
 
 -- Helpful keymap in vertical edit mode
-vim.keymap.set('i', '<C-c>', '<Esc>')
-
--- Keymaps for diffget. Useful when resolving conflicts
-vim.keymap.set('n', 'gh', '<cmd>diffget //2<cr>')
-vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>')
-
--- Netrw
-vim.g.netrw_banner = 0
-vim.keymap.set({ 'n', 'v' }, '\\', '<cmd>Lexplore<cr>', { desc = 'Toggle filetree' })
-vim.keymap.set('n', '<leader>fe', '<cmd>Explore<cr>', { desc = 'Open Netrw' })
-vim.g.netrw_winsize = 15
-vim.g.netrw_liststyle = 3
-
--- Move highlighted region up and down
-vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv")
+set('i', '<C-c>', '<Esc>')
 
 -- Zoom current window
-vim.keymap.set({ 'n', 'v' }, '<leader>z', '<cmd>lua MiniMisc.zoom()<cr>', { desc = 'Zoom current window' })
+set({ 'n', 'v' }, '<leader>z', '<cmd>lua MiniMisc.zoom()<cr>', { desc = 'Zoom current window' })
+
+-- SpaceX to make executable file
+set('n', '<leader>x', '<cmd>!chmod +x %<cr>', { silent = true })
+
+-- Cancel the highlighs
+set('n', '<C-c>', '<cmd>nohlsearch<cr>')
+
+-- Disable the worst key
+set('n', 'Q', '<nop>')
 
 -- Delete the line below and move the cursor to the left
-vim.keymap.set('n', 'J', "mzJ'z")
+set('n', 'J', "mzJ'z")
 
--- Centers the cursor within the window after jumping
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+-- Keymaps for diffget. Useful when resolving conflicts
+set('n', 'gh', '<cmd>diffget //2<cr>')
+set('n', 'gl', '<cmd>diffget //3<cr>')
 
--- Helpful for copy and paste
-vim.keymap.set('x', '<leader>p', '"_dP')
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>Y', '"+Y')
-vim.keymap.set('n', '<leader>d', '"_d')
-vim.keymap.set('v', '<leader>d', '"_d')
-
-vim.keymap.set('n', 'Q', '<nop>')
+-- Move highlighted region up and down
+set('v', 'J', ":m '>+1<cr>gv=gv")
+set('v', 'K', ":m '<-2<cr>gv=gv")
 
 -- Stay in indent mode
 set('v', '<', '<gv')
 set('v', '>', '>gv')
 
 -- Navigate between location list items
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<cr>zz', { desc = 'Next item in location list' })
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<cr>zz', { desc = 'Previous item in location list' })
+set('n', '<leader>k', '<cmd>lnext<cr>zz', { desc = 'Next item in location list' })
+set('n', '<leader>j', '<cmd>lprev<cr>zz', { desc = 'Previous item in location list' })
 
 -- Navigate between quickfix list items
-vim.keymap.set('n', '<leader>k', '<cmd>cnext<cr>zz', { desc = 'Next item in qfixlist' })
-vim.keymap.set('n', '<leader>j', '<cmd>cprev<cr>zz', { desc = 'Previous item in qfixlist' })
+set('n', '<leader>k', '<cmd>cnext<cr>zz', { desc = 'Next item in qfixlist' })
+set('n', '<leader>j', '<cmd>cprev<cr>zz', { desc = 'Previous item in qfixlist' })
 
--- SpaceX to make executable file
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<cr>', { silent = true })
+-- Centers the cursor within the window after jumping
+set('n', '<C-d>', '<C-d>zz')
+set('n', '<C-u>', '<C-u>zz')
+set('n', 'n', 'nzzzv')
+set('n', 'N', 'Nzzzv')
 
-vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<cr>')
+-- Netrw
+vim.g.netrw_banner = 0
+set({ 'n', 'v' }, '\\', '<cmd>Lexplore<cr>', { desc = 'Toggle filetree' })
+set('n', '<leader>fe', '<cmd>Explore<cr>', { desc = 'Open Netrw' })
+vim.g.netrw_winsize = 15
+vim.g.netrw_liststyle = 3
+
+-- Helpful for copy and paste
+set('x', '<leader>p', '"_dP')
+set('n', '<leader>y', '"+y')
+set('v', '<leader>y', '"+y')
+set('n', '<leader>Y', '"+Y')
+set('n', '<leader>d', '"_d')
+set('v', '<leader>d', '"_d')
+
+-- Keymaps for tabpages
+set('n', '<s-t>', '<cmd>tabnew<cr>', { desc = 'New tabpage' })
+set('n', '<s-c>', '<cmd>tabclose<cr>', { desc = 'Close current tabpage' })
+set('n', '<s-x>', '<cmd>tabonly<cr>', { desc = 'Close all other tabpages' })
+set('n', '<s-h>', '<cmd>tabp<cr>', { desc = 'Go to previous tabpage' })
+set('n', '<s-l>', '<cmd>tabn<cr>', { desc = 'Go to next tabpage' })
+
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
