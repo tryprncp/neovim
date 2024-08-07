f046688f3ee66984cdb20b646b871262ee75285c
tryprncp <tryprncp@gmail.com>
2024-08-07 09:22:19 +0800

    Move lazy setup in init.lua

--- init.lua
+++ init.lua
@@ -45,5 +45,22 @@ require 'tryprncp.autocommands'
 require 'tryprncp.keymaps'
 require 'tryprncp.options'
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Load plugins ]]
+require('lazy').setup {
+  spec = {
+    { import = 'plugins' },
+  },
+  ui = {
+    border = 'rounded',
+  },
+}
+
 -- vim: ts=2 sts=2 sw=2 et
