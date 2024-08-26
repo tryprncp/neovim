f4d780e6514a8431d386cea96ad77e7643343432
tryprncp <tryprncp@gmail.com>
2024-08-27 00:46:31 +0800

    Minor changes

--- lua/tryprncp/options.lua
+++ lua/tryprncp/options.lua
@@ -5,6 +5,9 @@ set.softtabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
 
+-- Disable mouse
+set.mouse = ''
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
